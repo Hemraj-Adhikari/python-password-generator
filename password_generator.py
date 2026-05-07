9225142a5e60d9d6b9ca6eaf9372a8439ea5bb91
Hemraj Adhikari <hemrajadhikari2025@gmail.com>
2026-05-07 12:15:11 +0545

    Add password generator script

---
+++ password_generator.py
@@ -0,0 +1,17 @@
+import random
+import string
+
+print("=== Password Generator ===")
+
+length = int(input("Enter password length: "))
+
+letters = string.ascii_letters
+numbers = string.digits
+symbols = string.punctuation
+
+all_characters = letters + numbers + symbols
+
+password = "".join(random.choice(all_characters) for _ in range(length))
+
+print("\nGenerated Password:")
+print(password)
